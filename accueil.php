9260f0d42bc29903dd26eea1a99261a8fa9e9ff3
Chiara LABIANCA <clabi@lmdsio.com>
2024-09-11 14:32:46 +0200

    Mon projet version 1

---
+++ accueil.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Accueil</title>
+	</head>
+	<body>
+		<p> Bonjour </p>
+		<?php 
+			echo $_POST["prenom"];
+			echo $_POST["nom"].'<br>';
+		?>
+	</body>
+</html>
